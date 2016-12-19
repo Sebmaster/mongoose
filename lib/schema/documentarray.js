Sebmaster/mongoose

commit cdd63bf6ff0854018cd6b70fd8397c9e942dcd59

Fix #2140: bad crash in documentarray.js

lib/schema/documentarray.js
@@ -139,7 +139,11 @@ DocumentArray.prototype.cast = function (value, doc, init, prev) {
         subdoc = new this.casterConstructor(null, value, true, selected);
         value[i] = subdoc.init(value[i]);
       } else {
-        if (prev && (subdoc = prev.id(value[i]._id))) {
+        try {
+          subdoc = prev.id(value[i]._id);
+        } catch(e) {}
+
+        if (prev && subdoc) {
           // handle resetting doc with existing id but differing data
           // doc.array = [{ doc: 'val' }]
           subdoc.set(value[i]);

test/model.test.js
@@ -139,6 +139,21 @@ describe('Model', function(){
     })
   });
 
+  it('gh-2140', function(done) {
+    var db = start();
+    var S = new Schema({
+      field: [{text: String}]
+    });
+
+    var Model = db.model('gh-2140', S, 'gh-2140');
+    var s = new Model();
+    s.field = [null];
+    s.field = [{text: 'text'}];
+
+    assert.ok(s.field[0]);
+    done();
+  });
+
   describe('schema', function(){
     it('should exist', function(done){
       var db = start()
@@ -4759,6 +4774,7 @@ describe('Model', function(){
       });
     });
 
+    
     it('Compound index on field earlier declared with 2dsphere index is saved', function (done) {
       var db = start();
       var PersonSchema = new Schema({

commit cedecfeed3098a744bcb1ec72f14a1e84e76fd89

Fix perf regression introduced in cdd63bf

lib/schema/documentarray.js
@@ -242,9 +242,16 @@ DocumentArray.prototype.cast = function(value, doc, init, prev, options) {
         subdoc = new this.casterConstructor(null, value, true, selected, i);
         value[i] = subdoc.init(value[i]);
       } else {
-        try {
-          subdoc = prev.id(value[i]._id);
-        } catch (e) {
+        if (prev != null) {
+          var isNull = false;
+          for (var j = 0; j < prev.length; ++j) {
+            if (prev[j] == null) {
+              isNull = true;
+            }
+          }
+          if (!isNull) {
+            subdoc = prev.id(value[i]._id);
+          }
         }
 
         if (prev && subdoc) {